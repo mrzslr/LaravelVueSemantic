da87aae5e09ae17cdbbe188bc7ec339ce8e29080
MohammadReza <frishter.ms@gmail.com>
2017-08-06 01:02:51 +0430

    make Movie model,migration and controller with resource. add Movie web route

---
+++ database/migrations/2017_08_05_201645_create_movies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title',250);
+            $table->text('cover');
+            $table->text('summary');
+            $table->float('rate');
+            $table->integer('year');
+            $table->string('genre',250);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
